f6bd342e9acc2933330631182a668b8939f72055
andres <ancues02@ucm.es>
2021-12-02 17:03:43 +0100

    hecho ejercicio 2

--- p5/p5_2.py
+++ p5/p5_2.py
@@ -2,8 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
+import math
 from scipy.io import loadmat
 
+# Calcula la funcion sigmoide 
+def sigmoide(z):
+    return 1/(1+math.e**(-z))
 
 #  Calcula el coste (regularizado)
 def costeReg(theta, X, Y , reg):
@@ -20,8 +24,8 @@ def costeReg(theta, X, Y , reg):
     coste += regularizacion
     return coste
 
-#  Calcula el gradiente (regularizado)
 
+#  Calcula el gradiente (regularizado)
 def gradiente(Theta, X, Y, _lambda):
     m=np.shape(X)[0]
     H=np.dot(X,Theta)
@@ -49,29 +53,44 @@ def trainit(Theta, X, Y, reg, nIter):
         options={'maxiter': nIter})
     return res
 
+# Calcula el error (similar al coste pero sin regularizacion)
+def error(theta, X, Y):
+    H = np.dot(X, theta)
+# Es el cuadrado porque son matrices unidimensionales
+    Aux = np.power((H - Y),2)
+    
+    coste = (1/ (2 * np.shape(X)[0])) * np.sum(Aux)
+    return coste
+
 def main():
 
     data = loadmat('ex5data1.mat')
     X=data["X"]
     X_1s = np.hstack([np.ones([np.shape(X)[0], 1]), X]) 
     Y=data["y"]
-   
+    Xval=data["Xval"]
+    Xval_1s = np.hstack([np.ones([np.shape(Xval)[0], 1]), Xval]) 
+    Yval=data["yval"]
+
     _lambda = 0
     y = np.ravel(Y)
     Theta = np.array([1,1])
-    res = trainit(Theta, X_1s, y, _lambda, 70)
-    print(res)
+    cValidationCoste = np.zeros(np.shape(X)[0] )
+    errorCoste = np.zeros(np.shape(X)[0] )
+    for i in range(1, np.shape(X)[0] + 1):
+        res = trainit(Theta, X_1s[0:i], y[0:i], _lambda, 70)
+        cValidationCoste[i - 1] = error(res.x, Xval_1s, np.ravel(Yval))
+        errorCoste[i - 1] = error(res.x, X_1s[0:i], y[0:i])
 
-    plt.plot(X, Y, "x", c='red')
-    min_x = min(X)
-    max_X = max(X)
-    min_y = res.x[0] + res.x[1] * min_x
-    max_Y = res.x[0] + res.x[1] * max_X
-    plt.plot([min_x, max_X], [min_y, max_Y], c='blue')
-    plt.ylabel("Water flowing out of the dam (y)")
-    plt.xlabel("Change water level (x)")
-    plt.savefig("resultado1.png")
-    
+
+ 
+    plt.plot(errorCoste, c='blue')
+
+    plt.ylabel("Error")
+    plt.xlabel("Change water level (x)Number of training examples")
+    plt.plot(cValidationCoste, c='orange')
+
+    plt.savefig("Resultado Error Training-Cross")
     
     #print(costeReg(Theta,X_1s,y,_lambda))
     #print(gradiente(Theta, X_1s,y,_lambda))
